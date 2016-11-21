5b7a7e8a7a90fd51e2a3a07a51ddda46165d3f20
rapurasusann <rapurasusann@yahoo.co.jp>
2016-11-21 16:03:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 onload =function(){
   var canvas = document.getElementById('canvas');
-  canvas.width =500；
+  canvas.width = 500;
   canvas.height = 300;
   
   var gl = canvas.getContext('webgl')||canvas.getContext('experimental-webgl');
